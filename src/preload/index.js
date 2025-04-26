57d2a248afee679fcff3af5372a17eb6b9ce16cc
rockyyyr <rockyrobson@hotmail.com>
2025-04-25 22:14:52 -0700

    testing

--- src/preload/index.js
+++ src/preload/index.js
@@ -20,7 +20,8 @@ if (process.contextIsolated) {
                 port = new SerialPort({
                     path: '/dev/serial0',
                     baudRate: 115200,
-                    lock: false
+                    lock: false,
+                    autoOpen: true
                 });
                 parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
