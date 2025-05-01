21c47af1d7177252c5d611ab2e941dd2051b8053
rockyyyr <rockyrobson@hotmail.com>
2025-04-30 21:17:49 -0700

    testing

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-exec chromium-browser \
- --noerrdialogs \
- --disable-infobars \
- --disable-web-security \
- --user-data-dir="/tmp/chrome-dev" \
- --allow-file-access-from-files \
- --overscroll-history-navigation=0 \
- --window-size=1920,550 \
- --window-position=0,0 \
- --app="http://10.0.0.173:3333"
- 
+cd /home/rocky/Apps/cnc-dro
+npm start
