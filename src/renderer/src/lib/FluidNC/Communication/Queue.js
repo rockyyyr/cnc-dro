1273a4e84bed2b95fb4382b123db3f2c34d19422
rockyyyr <rockyrobson@hotmail.com>
2025-06-02 18:06:06 -0700

    added automatic status messages after a realtime character

--- src/renderer/src/lib/FluidNC/Communication/Queue.js
+++ src/renderer/src/lib/FluidNC/Communication/Queue.js
@@ -57,6 +57,7 @@ export default class Queue {
     _queue = message => {
         if (this.IMMEDIATE.includes(message)) {
             this.comm.forceSend(message);
+            this.comm.forceSend([STATUS, STATUS, STATUS].join('\r\n'));
 
             if (message === RESET) {
                 this.clear();
