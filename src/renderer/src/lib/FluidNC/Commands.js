60c7053f1d2d91b26ad1c609e5842ab247340795
rockyyyr <rockyrobson@hotmail.com>
2025-06-02 18:42:37 -0700

    testing

--- src/renderer/src/lib/FluidNC/Commands.js
+++ src/renderer/src/lib/FluidNC/Commands.js
@@ -8,7 +8,7 @@ export const RUN = '$SD/Run=/';
 export const JOG = '$J=';
 export const WAIT_FOR_JOG = '\x10';
 
-export const FEED_OVERRIDE_RESET = '\x90';
+export const FEEDRATE_OVERRIDE_RESET = '\x90';
 export const FEEDRATE_OVERRIDE_PLUS = '\x91';
 export const FEEDRATE_OVERRIDE_MINUS = '\x92';
 
