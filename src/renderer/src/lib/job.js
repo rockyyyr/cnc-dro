bd99b35e11599455f411b139a2a48b29dfb93a18
rockyyyr <rockyrobson@hotmail.com>
2025-06-06 17:47:52 -0700

    testing

--- src/renderer/src/lib/job.js
+++ src/renderer/src/lib/job.js
@@ -1,4 +1,7 @@
 import { Comms, Commands } from './FluidNC';
 
 export const run = filename => Comms.send(Commands.RUN + filename);
-export const reset = () => Comms.send(Commands.RESET);
+export const reset = () => {
+    Comms.send(Commands.RESET);
+    Comms.send('M9');
+};
