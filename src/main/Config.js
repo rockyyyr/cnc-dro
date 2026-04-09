6295ebbba5b4b9073cb225ff0ac892d04063cb7e
rockyyyr <rockyrobson@hotmail.com>
2026-04-09 15:57:08 -0700

    added logging to config file loading

--- src/main/Config.js
+++ src/main/Config.js
@@ -6,9 +6,22 @@ export const load = () => {
     const path = Path.join(OS.homedir(), '.cnc.json');
 
     try {
-        return existsSync(path)
-            ? JSON.parse(readFileSync(path, 'utf8'))
-            : null;
+        if (existsSync(path)) {
+
+            const config = JSON.parse(readFileSync(path, 'utf8'));
+
+            if (config) {
+                console.log('Config loaded from file.');
+                return config;
+            } else {
+                console.log('Config empty.');
+                return null;
+            }
+
+        } else {
+            console.log('Config file not found.');
+            return null;
+        }
 
     } catch (error) {
         console.error(`Config not loaded from ${path}`);
