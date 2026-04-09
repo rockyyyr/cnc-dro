adb79706c7fcb88010243136235fd481172d9737
rockyyyr <rockyrobson@hotmail.com>
2026-04-09 16:23:33 -0700

    Updated start script

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ set -e
 
 cd /home/rocky/apps/cnc-dro || exit 1
 
-echo "PWD=$PWD" > /tmp/cnc-launch.log
-echo "PATH=$PATH" >> /tmp/cnc-launch.log
-which node >> /tmp/cnc-launch.log 2>&1
-which npm >> /tmp/cnc-launch.log 2>&1
+echo "PWD=$PWD"
+echo "PATH=$PATH"
+which node
+which npm
 
-/usr/bin/npm start >> /tmp/cnc-launch.log 2>&1
+exec /usr/bin/npm start
